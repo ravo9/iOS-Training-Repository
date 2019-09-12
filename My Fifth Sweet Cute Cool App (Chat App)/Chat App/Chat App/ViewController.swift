9fdbf4bc65007ec7b8b50c9862ab4b16faf013d8
rafal.ozog <rafal.ozog@ocado.com>
2019-09-12 10:21:47 +0200

    Project upload

---
+++ My Fifth Sweet Cute Cool App (Chat App)/Chat App/Chat App/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Chat App
+//
+//  Created by Rafal Ozog on 12/09/2019.
+//  Copyright © 2019 Rafal Learning Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messagesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
